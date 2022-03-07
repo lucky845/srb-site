9283ee430a3a2cf9f1f872c02f685340f2c704aa
lucky845 <lc1447545564@gmail.com>
2022-03-07 10:09:05 +0800

    初始化

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- 公共头部 -->
+    <app-header />
+    <!-- 页面占位符 -->
+    <Nuxt />
+    <!-- 公共尾部 -->
+    <app-footer />
+  </div>
+</template>
+<script>
+import '~/assets/css/common.css'
+import AppHeader from '~/components/AppHeader'
+import AppFooter from '~/components/AppFooter'
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+}
+</script>
---
+++ pages/user.vue
@@ -0,0 +1,47 @@
+<template>
+  <!--个人中心-->
+  <div class="wrapper wbgcolor">
+    <div class="w1200 personal">
+      <div class="credit-ad">
+        <img src="~/assets/images/clist1.jpg" width="1200" height="96" />
+      </div>
+      <div class="personal-left">
+        <!-- 投资人页面路由 -->
+        <InvestorNav v-if="userType === 1" />
+        <!-- 借款人页面路由 -->
+        <BorrowerNav v-if="userType === 2" />
+      </div>
+      <!-- 嵌入页面 -->
+      <nuxt-child />
+      <div class="clear"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import '~/assets/css/user.css'
+import InvestorNav from '~/components/InvestorNav'
+import BorrowerNav from '~/components/BorrowerNav'
+import cookie from 'js-cookie'
+
+export default {
+  components: {
+    InvestorNav,
+    BorrowerNav,
+  },
+
+  data() {
+    return {
+      userType: 0,
+    }
+  },
+
+  mounted() {
+    let userInfo = cookie.get('userInfo')
+    if (userInfo) {
+      userInfo = JSON.parse(userInfo)
+      this.userType = userInfo.userType
+    }
+  },
+}
+</script>
