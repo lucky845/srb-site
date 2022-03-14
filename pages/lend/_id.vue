d105ffafe2ff5ea260d7fd6f93fb699e0df95195
lucky845 <lc1447545564@gmail.com>
2022-03-14 20:56:35 +0800

    显示投资记录

--- pages/lend/_id.vue
+++ pages/lend/_id.vue
@@ -399,9 +399,15 @@ export default {
     // 通过lenId获取标的信息
     let response = await $axios.$get('/api/core/lend/show/' + lendId)
 
+    // 投资记录
+    let responseLendItemList = await $axios.$get(
+      '/api/core/lendItem/list/' + lendId
+      )
+
     return {
       lend: response.data.lendDetail.lend, //标的详情
-      borrower: response.data.lendDetail.borrower //借款人信息
+      borrower: response.data.lendDetail.borrower, //借款人信息
+      lendItemList: responseLendItemList.data.list, //投资记录
     }
   },
 
